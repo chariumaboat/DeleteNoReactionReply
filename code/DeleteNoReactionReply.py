d0319dac6c92c4760ef94906639097b32ac4f298
mo mo <mailhasimasen@nomail.com>
2021-09-18 16:17:48 +0900

    error control

--- code/DeleteNoReactionReply.py
+++ code/DeleteNoReactionReply.py
@@ -10,39 +10,51 @@ access_secret = os.environ['AccessSecretVar']
 # Tweepy Auth
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_key, access_secret)
-api = tweepy.API(auth)
+api = tweepy.API(auth, wait_on_rate_limit=False)
 
-myReplyTweet = []
-mentionInReplyTweet = []
+noFavRtTweet = []
+mentionTweet = []
 noDeleteTweet = []
-deleteTweet = []
 
 
 def lambda_handler(event, context):
-    # Get My Reply
-    print("==========Get My Reply==========")
-    for tweet in tweepy.Cursor(api.user_timeline,exclude_replies = False,include_rts = False).items(200):
-        if "@" in tweet.text and tweet.favorite_count == 0 and tweet.retweet_count == 0:
-            print(tweet.id,tweet.created_at,tweet.text.replace('\n',''),tweet.favorite_count,tweet.retweet_count)
-            myReplyTweet.append(tweet.id)
-    
-    # Get in_reply_to_status_id in mentions
+    # No Favorite No RT Tweet
+    print("==========No Favorite No RT Tweet==========")
+    for tweet in tweepy.Cursor(api.user_timeline, exclude_replies=True).items(200):
+        if tweet.favorite_count == 0 and tweet.retweet_count == 0:
+            print(tweet.id, tweet.created_at, tweet.text.replace('\n', ''))
+            noFavRtTweet.append(tweet.id)
+
+    # Reply Tweet
     print("==========Reply Tweet==========")
     for mentions in tweepy.Cursor(api.mentions_timeline).items(400):
-        print(mentions.id,mentions.created_at,mentions.text.replace('\n',''))
-        mentionInReplyTweet.append(mentions.in_reply_to_status_id)
-    
+        print(mentions.id, mentions.created_at,
+              mentions.text.replace('\n', ''))
+        mentionTweet.append(mentions.in_reply_to_status_id)
+
+    print("==========No Favorite No RT Reply Tweet==========")
+    # No Favorite No RT Reply Tweet
+    for tweet in noFavRtTweet:
+        for reptw in mentionTweet:
+            if tweet == reptw:
+                try:
+                    print(api.get_status(tweet)._json)
+                except Exception as e:
+                    print(e)
+                else:
+                    noDeleteTweet.append(tweet)
+
     # Extraction Delete Tweet
     print("==========Extraction Delete tweet==========")
-    for mntnrptw in mentionInReplyTweet:
-        for myrptw in myReplyTweet:
-            if mntnrptw == myrptw:
-                deleteTweet.append(myrptw)
-    preparateDeleteTweet = set(myReplyTweet) ^ set(deleteTweet)
-    print(list(preparateDeleteTweet))
-    
+    perfectList = set(noFavRtTweet) ^ set(noDeleteTweet)
+    print(list(perfectList))
+
     # Delete Tweet
     print("==========delete tweet==========")
-    for deltw in preparateDeleteTweet:
-        print(api.get_status(deltw).id,api.get_status(deltw).created_at,api.get_status(deltw).text)
-        api.destroy_status(deltw)
+    for deltw in perfectList:
+        try:
+            print(api.get_status(deltw)._json)
+        except Exception as e:
+            print(e)
+        else:
+            api.destroy_status(deltw)
